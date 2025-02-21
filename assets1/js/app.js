e456cca0a5308ab50b6d1ff8e7c839efc7dc6fa3
Samboterham <berghoefsam79@gmail.com>
2025-02-21 10:51:36 +0100

    mooier gemaakt
    lettertype en hover effect mooier gemaakt.

---
+++ assets1/js/app.js
@@ -0,0 +1,20 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+}
+
+let count = 0; // Startwaarde
+
+// Selecteer alle knoppen met class "addToCart"
+document.querySelectorAll(".addtocart").forEach(button => {
+    button.addEventListener("click", function() {
+        count++; // Verhoog teller
+        document.getElementById("counter").textContent = count; // Update shoppingcart teller
+    });
+});
+
+
+   
---
+++ assets1/js/login.js
@@ -0,0 +1,4 @@
+document.getElementById("login_button").addEventListener("click", function() {
+    document.getElementById("naam_balk").value = "";
+    document.getElementById("naam_balk1").value = "";
+});
